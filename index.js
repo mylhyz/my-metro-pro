d884a2fefaf5d897dfd4eb04d0a57e35f6e004ca
mylhyz <mylhyz@gmail.com>
2020-10-02 18:15:52 +0800

    support custome jsx element

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import TestRenderer from 'react-test-renderer';
 
-function Link(props) {
-    return <a href={props.page}>{props.children}</a>;
+class View extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <view {...this.props}>{this.props.children}</view>
+        );
+    }
+}
+
+View.propTypes = {
+    style: PropTypes.object
+}
+
+class Text extends Component {
+
+    render() {
+        return (
+            <text {...this.props}>{this.props.children}</text>
+        );
+    }
+}
+
+Text.propTypes = {
+    ...View.propTypes
+}
+
+class Image extends Component {
+
+    render() {
+        return (
+            <image {...this.props}>{this.props.children}</image>
+        );
+    }
+}
+
+Image.propTypes = {
+    ...View.propTypes,
+    uri: PropTypes.string
 }
 
 const testRenderer = TestRenderer.create(
-    <Link page="https://www.facebook.com/">Facebook</Link>
+    <View style={{ flex: 1 }}>
+        <Image uri={"https://reactnative.dev/img/header_logo.svg"} />
+        <Text>Hello,React</Text>
+    </View>
 );
 
 let json = JSON.stringify(testRenderer.toJSON());
--- metro-bundle.js
+++ metro-bundle.js
@@ -215,25 +215,130 @@ var __BUNDLE_START_TIME__=this.nativePerformanceNow?nativePerformanceNow():Date.
 __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
   "use strict";
 
-  var _react = _interopRequireDefault(_$$_REQUIRE(_dependencyMap[0]));
+  var _react = _interopRequireWildcard(_$$_REQUIRE(_dependencyMap[0]));
 
-  var _reactTestRenderer = _interopRequireDefault(_$$_REQUIRE(_dependencyMap[1]));
+  var _propTypes = _interopRequireDefault(_$$_REQUIRE(_dependencyMap[1]));
+
+  var _reactTestRenderer = _interopRequireDefault(_$$_REQUIRE(_dependencyMap[2]));
 
   function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-  function Link(props) {
-    return _react["default"].createElement("a", {
-      href: props.page
-    }, props.children);
-  }
+  function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
 
-  var testRenderer = _reactTestRenderer["default"].create(_react["default"].createElement(Link, {
-    page: "https://www.facebook.com/"
-  }, "Facebook"));
+  function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+  function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+  function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+  function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+  function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+  function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+  function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+  function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+  function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+  function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+  function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+  function _assertThisInitialized(self) { if (self === undefined) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+  function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+  function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+  var View = function (_Component) {
+    _inherits(View, _Component);
+
+    var _super = _createSuper(View);
+
+    function View(props) {
+      _classCallCheck(this, View);
+
+      return _super.call(this, props);
+    }
+
+    _createClass(View, [{
+      key: "render",
+      value: function render() {
+        return _react["default"].createElement("view", this.props, this.props.children);
+      }
+    }]);
+
+    return View;
+  }(_react.Component);
+
+  View.propTypes = {
+    style: _propTypes["default"].object
+  };
+
+  var Text = function (_Component2) {
+    _inherits(Text, _Component2);
+
+    var _super2 = _createSuper(Text);
+
+    function Text() {
+      _classCallCheck(this, Text);
+
+      return _super2.apply(this, arguments);
+    }
+
+    _createClass(Text, [{
+      key: "render",
+      value: function render() {
+        return _react["default"].createElement("text", this.props, this.props.children);
+      }
+    }]);
+
+    return Text;
+  }(_react.Component);
+
+  Text.propTypes = _objectSpread({}, View.propTypes);
+
+  var Image = function (_Component3) {
+    _inherits(Image, _Component3);
+
+    var _super3 = _createSuper(Image);
+
+    function Image() {
+      _classCallCheck(this, Image);
+
+      return _super3.apply(this, arguments);
+    }
+
+    _createClass(Image, [{
+      key: "render",
+      value: function render() {
+        return _react["default"].createElement("image", this.props, this.props.children);
+      }
+    }]);
+
+    return Image;
+  }(_react.Component);
+
+  Image.propTypes = _objectSpread(_objectSpread({}, View.propTypes), {}, {
+    uri: _propTypes["default"].string
+  });
+
+  var testRenderer = _reactTestRenderer["default"].create(_react["default"].createElement(View, {
+    style: {
+      flex: 1
+    }
+  }, _react["default"].createElement(Image, {
+    uri: "https://reactnative.dev/img/header_logo.svg"
+  }), _react["default"].createElement(Text, null, "Hello,React")));
 
   var json = JSON.stringify(testRenderer.toJSON());
   print_log(json);
-},0,[1,4]);
+},0,[1,4,7]);
 __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
   'use strict';
 
@@ -746,12 +851,81 @@ __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, e
   };
 },3,[]);
 __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
+  "use strict";
+
+  {
+    module.exports = _$$_REQUIRE(_dependencyMap[0])();
+  }
+},4,[5]);
+__d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
+  'use strict';
+
+  var ReactPropTypesSecret = _$$_REQUIRE(_dependencyMap[0]);
+
+  function emptyFunction() {}
+
+  function emptyFunctionWithReset() {}
+
+  emptyFunctionWithReset.resetWarningCache = emptyFunction;
+
+  module.exports = function () {
+    function shim(props, propName, componentName, location, propFullName, secret) {
+      if (secret === ReactPropTypesSecret) {
+        return;
+      }
+
+      var err = new Error("Calling PropTypes validators directly is not supported by the `prop-types` package. Use PropTypes.checkPropTypes() to call them. Read more at http://fb.me/use-check-prop-types");
+      err.name = 'Invariant Violation';
+      throw err;
+    }
+
+    ;
+    shim.isRequired = shim;
+
+    function getShim() {
+      return shim;
+    }
+
+    ;
+    var ReactPropTypes = {
+      array: shim,
+      bool: shim,
+      func: shim,
+      number: shim,
+      object: shim,
+      string: shim,
+      symbol: shim,
+      any: shim,
+      arrayOf: getShim,
+      element: shim,
+      elementType: shim,
+      instanceOf: getShim,
+      node: shim,
+      objectOf: getShim,
+      oneOf: getShim,
+      oneOfType: getShim,
+      shape: getShim,
+      exact: getShim,
+      checkPropTypes: emptyFunctionWithReset,
+      resetWarningCache: emptyFunction
+    };
+    ReactPropTypes.PropTypes = ReactPropTypes;
+    return ReactPropTypes;
+  };
+},5,[6]);
+__d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
+  'use strict';
+
+  var ReactPropTypesSecret = 'SECRET_DO_NOT_PASS_THIS_OR_YOU_WILL_BE_FIRED';
+  module.exports = ReactPropTypesSecret;
+},6,[]);
+__d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
   'use strict';
 
   {
     module.exports = _$$_REQUIRE(_dependencyMap[0]);
   }
-},4,[5]);
+},7,[8]);
 __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
   /** @license React v16.13.1
    * react-test-renderer.production.min.js
@@ -5405,14 +5579,14 @@ __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, e
   });
 
   module.exports = Cf.default || Cf;
-},5,[3,6,1,8]);
+},8,[3,9,1,11]);
 __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
   'use strict';
 
   {
     module.exports = _$$_REQUIRE(_dependencyMap[0]);
   }
-},6,[7]);
+},9,[10]);
 __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
   /** @license React v0.19.1
    * scheduler-unstable_mock.production.min.js
@@ -5790,14 +5964,14 @@ __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, e
   exports.unstable_yieldValue = function (a) {
     null === l ? l = [a] : l.push(a);
   };
-},7,[]);
+},10,[]);
 __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
   'use strict';
 
   {
     module.exports = _$$_REQUIRE(_dependencyMap[0]);
   }
-},8,[9]);
+},11,[12]);
 __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
   /** @license React v0.19.1
    * scheduler.production.min.js
@@ -6155,5 +6329,5 @@ __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, e
       }
     };
   };
-},9,[]);
+},12,[]);
 __r(0);
