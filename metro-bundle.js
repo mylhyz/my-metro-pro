67d9468707708ff7c1214277d8afd3962d1fd5fc
mylhyz <mylhyz@gmail.com>
2020-10-06 22:20:38 +0800

    support flexDirection

--- metro-bundle.js
+++ metro-bundle.js
@@ -6201,9 +6201,7 @@ __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, e
 
   var _View = _interopRequireDefault(_$$_REQUIRE(_dependencyMap[1]));
 
-  var _Image = _interopRequireDefault(_$$_REQUIRE(_dependencyMap[2]));
-
-  var _Text = _interopRequireDefault(_$$_REQUIRE(_dependencyMap[3]));
+  var _Text = _interopRequireDefault(_$$_REQUIRE(_dependencyMap[2]));
 
   function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -6249,27 +6247,45 @@ __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, e
       value: function render() {
         return _react["default"].createElement(_View["default"], {
           style: {
-            flex: 1
+            flexDirection: 'column'
           }
         }, _react["default"].createElement(_View["default"], {
           style: {
-            width: 48,
-            height: 48,
+            width: 96,
+            height: 96,
             backgroundColor: '#ffff0000'
           }
-        }), _react["default"].createElement(_View["default"], {
+        }, _react["default"].createElement(_Text["default"], null, "1")), _react["default"].createElement(_View["default"], {
           style: {
-            width: 48,
-            height: 48,
+            width: 96,
+            height: 96,
             backgroundColor: '#ff00ff00'
           }
-        }), _react["default"].createElement(_View["default"], {
+        }, _react["default"].createElement(_Text["default"], null, "2")), _react["default"].createElement(_View["default"], {
           style: {
-            width: 48,
-            height: 48,
+            width: 96,
+            height: 96,
             backgroundColor: '#ff0000ff'
           }
-        }));
+        }, _react["default"].createElement(_Text["default"], null, "3")), _react["default"].createElement(_View["default"], {
+          style: {
+            width: 96,
+            height: 96,
+            backgroundColor: '#ffff0000'
+          }
+        }, _react["default"].createElement(_Text["default"], null, "1")), _react["default"].createElement(_View["default"], {
+          style: {
+            width: 96,
+            height: 96,
+            backgroundColor: '#ff00ff00'
+          }
+        }, _react["default"].createElement(_Text["default"], null, "2")), _react["default"].createElement(_View["default"], {
+          style: {
+            width: 96,
+            height: 96,
+            backgroundColor: '#ff0000ff'
+          }
+        }, _react["default"].createElement(_Text["default"], null, "3")));
       }
     }]);
 
@@ -6277,7 +6293,7 @@ __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, e
   }(_react.Component);
 
   exports["default"] = App;
-},10,[1,11,15,16]);
+},10,[1,11,15]);
 __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
   "use strict";
 
@@ -6461,80 +6477,6 @@ __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, e
 
   function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
-  var Image = function (_Component) {
-    _inherits(Image, _Component);
-
-    var _super = _createSuper(Image);
-
-    function Image(props) {
-      _classCallCheck(this, Image);
-
-      return _super.call(this, props);
-    }
-
-    _createClass(Image, [{
-      key: "render",
-      value: function render() {
-        return _react["default"].createElement("image", this.props, this.props.children);
-      }
-    }]);
-
-    return Image;
-  }(_react.Component);
-
-  exports["default"] = Image;
-  Image.propTypes = _objectSpread(_objectSpread({}, _View["default"].propTypes), {}, {
-    uri: _propTypes["default"].string
-  });
-},15,[1,12,11]);
-__d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, exports, _dependencyMap) {
-  "use strict";
-
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
-  exports["default"] = undefined;
-
-  var _react = _interopRequireWildcard(_$$_REQUIRE(_dependencyMap[0]));
-
-  var _propTypes = _interopRequireDefault(_$$_REQUIRE(_dependencyMap[1]));
-
-  var _View = _interopRequireDefault(_$$_REQUIRE(_dependencyMap[2]));
-
-  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-  function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
-
-  function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-
-  function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-  function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-  function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-  function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-  function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-  function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-  function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-  function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-  function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-  function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-  function _assertThisInitialized(self) { if (self === undefined) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-  function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-  function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
   var Text = function (_Component) {
     _inherits(Text, _Component);
 
@@ -6561,5 +6503,5 @@ __d(function (global, _$$_REQUIRE, _$$_IMPORT_DEFAULT, _$$_IMPORT_ALL, module, e
     textSize: _propTypes["default"].number,
     textColor: _propTypes["default"].string
   });
-},16,[1,12,11]);
+},15,[1,12,11]);
 __r(0);
--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 
 import View from '../Framework/Component/View';
-import Image from '../Framework/Component/Image';
+// import Image from '../Framework/Component/Image';
 import Text from '../Framework/Component/Text';
 
 export default class App extends Component {
     render() {
         return (
-            <View style={{ flex: 1 }}>
-                <View style={{ width: 48, height: 48, backgroundColor: '#ffff0000' }}></View>
-                <View style={{ width: 48, height: 48, backgroundColor: '#ff00ff00' }}></View>
-                <View style={{ width: 48, height: 48, backgroundColor: '#ff0000ff' }}></View>
+            <View style={{ flexDirection: 'column' }}>
+                <View style={{ width: 96, height: 96, backgroundColor: '#ffff0000' }}>
+                    <Text>1</Text>
+                </View>
+                <View style={{ width: 96, height: 96, backgroundColor: '#ff00ff00' }}>
+                    <Text>2</Text>
+                </View>
+                <View style={{ width: 96, height: 96, backgroundColor: '#ff0000ff' }}>
+                    <Text>3</Text>
+                </View>
+
+                <View style={{ width: 96, height: 96, backgroundColor: '#ffff0000' }}>
+                    <Text>1</Text>
+                </View>
+                <View style={{ width: 96, height: 96, backgroundColor: '#ff00ff00' }}>
+                    <Text>2</Text>
+                </View>
+                <View style={{ width: 96, height: 96, backgroundColor: '#ff0000ff' }}>
+                    <Text>3</Text>
+                </View>
+
                 {/* <Image uri={"https://hackernoon.com/images/z2xg2bpo.jpg"} /> */}
                 {/* <Text textSize={48} textColor={'#FF0000FF'}>Hello,React</Text> */}
             </View>
